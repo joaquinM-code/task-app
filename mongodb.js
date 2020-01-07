ab6f9c8f609f8e532747a22a2a8863b56a84413a
ecatom <netpassrecovers@hotmail.com>
2020-01-07 17:51:50 +0100

    First test mongodb

---
+++ mongodb.js
@@ -0,0 +1,20 @@
+// CRUD operations (Create, Read, Update and Delete)
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true , useUnifiedTopology:true} , (error , client)=>{
+    if(error){
+        return console.log('Unable to connect the database')
+    }
+
+    const db = client.db(databaseName)
+    db.collection('users').insertOne({
+        name: "Ecatom",
+        age: 32
+    })
+
+})
